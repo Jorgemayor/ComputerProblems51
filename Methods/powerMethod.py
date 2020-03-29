0b5b98cb8fb7fc7f334387d4975143c4601ba8dc
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-29 00:06:12 -0500

    Power method implemented

--- Methods/powerMethod.py
+++ Methods/powerMethod.py
@@ -1,5 +1,13 @@
 import numpy as np
 
-def apply(matrix, vector, steps):
-	print("It's here")
+def apply(matrixArr, vector, steps):
+	np.set_printoptions(precision=5)
+	x = np.array(vector)
+	matrix = np.array(matrixArr)
+	print("Step 0\n  vector:",x)
 
+	for i in range(1, steps+1):
+		y = matrix.dot(x)
+		r = np.amax(y)/np.amax(x)
+		x = y/np.amax(y)
+		print("\nStep",i,"\n  vector:",x,"\n  EigenValue:",r)
