ba0f0e2bff81e0861a05c5acf7177adf1dab7c5a
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-29 02:12:26 -0500

    Eigenvalue returned

--- Methods/powerMethod.py
+++ Methods/powerMethod.py
@@ -5,9 +5,11 @@ def apply(matrixArr, vector, steps):
 	x = np.array(vector)
 	matrix = np.array(matrixArr)
 	print("Step 0\n  vector:",x)
-
+	
+	r=None
 	for i in range(1, steps+1):
 		y = matrix.dot(x)
 		r = np.amax(y)/np.amax(x)
 		x = y/np.amax(y)
 		print("\nStep",i,"\n  Vector:",x,"\n  Eigenvalue:",r)
+	return r
