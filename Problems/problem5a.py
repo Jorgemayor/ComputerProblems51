01b15afa05c8d5fda0a9de1a3e52f832cc6cbb85
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-29 02:10:48 -0500

    Problem 5 solved

--- Problems/problem5a.py
+++ Problems/problem5a.py
@@ -3,7 +3,12 @@ sys.path.append('../')
 from Methods import inversePowerMethod as ipm
 
 def solve():
-	ipm.apply()
+	matrix = [[6, 5, -5],
+			  [2, 6, -2],
+			  [2, 5, -1]]
+	vector = [3, 7, -13]
+	steps = 11
+	print("\nSmallest eigenvalue:",ipm.apply(matrix, vector, steps))
 
 if __name__ == "__main__":
     solve()
