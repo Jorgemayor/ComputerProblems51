fe7ae24c0f849db5adca9e4c8775af0228ee24a2
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-28 23:17:55 -0500

    minor: indentation

--- Problems/problem1.py
+++ Problems/problem1.py
@@ -2,8 +2,8 @@ from Methods import powerMethod as pm
 
 def solve():
 	matrix = '6 5 -5; \
-		  2 6 -2; \
-		  2 5 -1'
+			  2 6 -2; \
+			  2 5 -1'
 	vector = '1 2 3'
 	steps = 100
 	pm.apply(matrix, vector, steps)
