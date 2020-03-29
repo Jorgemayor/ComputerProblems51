dde959cf6220b3e3a5115ac2631e5861b6e8fa21
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-29 00:05:50 -0500

    Data adapted

--- Problems/problem2.py
+++ Problems/problem2.py
@@ -1,9 +1,9 @@
 from Methods import powerMethod as pm
 
 def solve():
-	matrix = '6 5 -5; \
-			  2 6 -2; \
-			  2 5 -1'
+	matrix = [[6, 5, -5],
+			  [2, 6, -2],
+			  [2, 5, -1]]
 	vector = [-1, 1, 1]
 	steps = 28
 	pm.apply(matrix, vector, steps)
