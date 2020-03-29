2980b14fa7f58930f16db9d343739340a32bd521
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-28 23:13:15 -0500

    Data of problem added

--- Problems/problem1.py
+++ Problems/problem1.py
@@ -1,7 +1,12 @@
 from Methods import powerMethod as pm
 
 def solve():
-	pm.apply()
+	matrix = '6 5 -5; \
+		  2 6 -2; \
+		  2 5 -1'
+	vector = '1 2 3'
+	steps = 100
+	pm.apply(matrix, vector, steps)
 
 if __name__ == "__main__":
     solve()
