cac6612dc5a8e7c216bb6bf124eb29c2ea4bc8ea
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-29 02:12:05 -0500

    Inverse power method working

--- Methods/inversePowerMethod.py
+++ Methods/inversePowerMethod.py
@@ -1,4 +1,9 @@
+from . import powerMethod as pw
 import numpy as np
 
-def apply():
-	print("It's here")
+def apply(matrixArr, vector, steps):
+	matrix = np.array(matrixArr)
+	invertedMatrix = np.linalg.inv(matrix)
+	u = pw.apply(invertedMatrix, vector, steps)
+	r = 1/u
+	return r
