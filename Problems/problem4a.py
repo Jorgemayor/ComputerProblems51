ffc6cd3a79046eb162d8e6180e88940100c0f2cf
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-03-29 01:28:58 -0500

    Individual execution fixed

--- Problems/problem4a.py
+++ Problems/problem4a.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../')
 from Methods import powerMethod as pm
 
 def solve():
--- Problems/problem5a.py
+++ Problems/problem5a.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../')
 from Methods import inversePowerMethod as ipm
 
 def solve():
